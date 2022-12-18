e6d56cbf83496e4dcb3c41d17c4917d8175deb26
guido <ruizbenitezguido11@gmail.com>
2022-12-18 00:26:28 -0300

    actualizado

--- vista/modulos/reportes.php
+++ vista/modulos/reportes.php
@@ -29,7 +29,7 @@
         <div class="row">
 
 
-           <div class="col-md-6 col-xs-12">
+           <div class="col-md-8 col-xs-12">
              
             <?php
 
@@ -39,7 +39,7 @@
 
            </div>
 
-            <div class="col-md-6 col-xs-12">
+            <div class="col-md-4 col-xs-12">
              
             <?php
 
