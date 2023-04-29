f6c34bd42cc8b3b7a08e476c189cd30cf6289bc6
Guido Ruiz <ruizbenitezguido11@gmail.com>
2023-04-29 16:26:24 -0400

    actualizado

--- modelo/puntero.modelo.php
+++ modelo/puntero.modelo.php
@@ -31,7 +31,7 @@ class ModeloPuntero{
 					inner join personas as per
 					on pun.id_persona_puntero = per.id_persona  
 					order by id_puntero desc
-					limit 2000
+					limit 10
 				");
 
 			$stmt -> execute();
